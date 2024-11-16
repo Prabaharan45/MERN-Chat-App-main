23ef163cfd110bbe928135d09bb78577a248a41b
Prabaharan45 <prabaharant7@gmail.com>
2024-11-16 12:04:39 +0530

    first commit

---
+++ MERN-Chat-App-main/server/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/userRoutes");
+const chatRouter = require("./routes/chatRoutes");
+const messageRouter = require("./routes/messageRoutes");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+// MongoDB connection
+mongoose.connect("mongodb+srv://prabaharant45:12345@chatapp.ivsmx.mongodb.net/?retryWrites=true&w=majority&appName=chatapp").then(()=>{
+  console.log("mongo db connected");
+}).catch(err=>{
+  console.log(err);
+  
+})
+
+// JSON
+app.use(express.json());
+
+// CORS usage
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+
+// Route middleware
+app.use("/api/auth", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+
+const server = app.listen(5000, console.log("Server listening on port 5000"));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+    console.log("User connected", userData._id);
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("Chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", (userData) => {
+    socket.leave(userData._id);
+  });
+});
